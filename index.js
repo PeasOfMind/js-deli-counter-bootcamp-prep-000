b43d1d3289d5973359d0a4e30f188f42c2eadf06
Yeqing Liu <sunsetpeak42@gmail.com>
2018-07-14 03:38:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(katzDeliLine){
   if(!katzDeliLine) return "There is nobody waiting to be served!"
-  return `Currently serving ${katzDeliLine.shift()}`;
+  return `Currently serving ${katzDeliLine.shift()}.`;
 }
 
 function currentLine(katzDeliLine){
