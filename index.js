873abb9fa41c0eb01dbd69678aaffcad8ce3a3cf
Yeqing Liu <sunsetpeak42@gmail.com>
2018-07-14 03:30:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ function takeANumber(katzDeliLine, name){
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing(){
+function nowServing(katzDeliLine){
   if(katzDeliLine.length = 0) return "There is nobody waiting to be served!"
   return katzDeliLine.shift();
 }
 
-function currentLine(){
+function currentLine(katzDeliLine){
   if (katzDeliLine.length = 0) return "The line is currently empty."
   katzString = "The line is currently:"
   for(let i = 1; i<= katzDeliLine.length; i++){
